74993595b53df718fb6c2d7ce272a011243eb666
aneesamedina <aneesamedina13@gmail.com>
2025-09-03 22:01:41 -0700

    pause play 2

--- client/src/App.js
+++ client/src/App.js
@@ -116,7 +116,7 @@ function MainQueueApp({ role }) {
 
   const playNext = async () => {
     try {
-      const res = await fetch(`${API_URL}/host/next`, { method: 'POST', credentials: 'include' });
+      const res = await fetch(`${API_URL}/play`, { method: 'POST', credentials: 'include' });
       if (!res.ok) throw new Error(`HTTP error ${res.status}`);
       const data = await res.json();
       setNowPlaying(normalizeNowPlaying(data.nowPlaying));
